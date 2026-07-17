7464b740fafe8e0dec3e9fc448ad833349b02e2f
WJag <wjag@onet.pl>
2026-07-17 21:21:17 +0200

    Create homepage structure and first patterns2

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+
+function estate_theme_assets() {
+
+    wp_enqueue_style(
+        'estate-theme-base',
+        get_template_directory_uri() . '/assets/css/base.css',
+        array(),
+        '1.0'
+    );
+
+}
+
+add_action(
+    'wp_enqueue_scripts',
+    'estate_theme_assets'
+);
